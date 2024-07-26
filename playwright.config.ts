84436325b2f2d828ab3b7d2ba15e17220d9bd902
jeanljh <leejeanhwa@gmail.com>
2024-07-26 16:34:31 +0800

    refactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://v4.testnet.dydx.exchange/',
     /* Run tests in headless */
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
