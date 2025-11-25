c198812c911b079e8afcf7aa3cd6289d0ccc38e5
jeanljh <jeanljh@users.noreply.github.com>
2025-11-25 03:33:33 +0000

    Deploying to gh-pages from @ jeanljh/pw-frontend@f288a527dab13a38d4f0c05fadea94839d34397a 🚀

--- tests/helpers/utils.ts
+++
@@ -1,9 +0,0 @@
-export default class Utils {
-	static wait = async (duration: number) => new Promise(resolve => setTimeout(resolve, duration));
-	static formatChainAddress(address: string) {
-    const prefix = address.slice(0, 8)
-    const suffix = address.slice(-4)
-    const middle = '.'.repeat(3)
-    return prefix + middle + suffix
-  }
-}
